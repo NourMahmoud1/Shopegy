49be2d87a789ab1dd0c559c4ba69768914cd4083
Andrew Mekhail <s24513432eg@std.aou.edu.eg>
2025-03-19 19:29:03 +0200

    Add ShopegyAppContext, Shipping, and ShippingAddress classes
    Added ShopegyAppContext class in the Data namespace, inheriting from
    DbContext, with DbSet properties for Shipping and ShippingAddress
    entities. Introduced Shipping class in the Models namespace with
    properties for shipping details, including a typo in ShppingAddressId.
    Added ShippingAddress class in the Models namespace with properties
    for address details.

---
+++ Shopegy/Data/ShopegyAppContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Models;
+
+namespace Data;
+
+public class ShopegyAppContext : DbContext
+{
+    public ShopegyAppContext(DbContextOptions<ShopegyAppContext> options) : base(options)
+    {
+
+    }
+    public DbSet<Shipping> Shipping { get; set; }
+    public DbSet<ShippingAddress> ShippingAddress { get; set; }
+}
---
+++ Shopegy/Models/Shipping.cs
@@ -0,0 +1,11 @@
+﻿namespace Models;
+
+ public class Shipping
+{
+    public int ShippingId { get; set; }
+    public int OrderId { get; set; }
+    public string ShppingAddressId { get; set; }
+    public string ShippingDate { get; set; }
+    public int TrackingNumber { get; set; }
+    public string Status { get; set; }
+}
