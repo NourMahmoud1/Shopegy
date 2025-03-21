4ac714160281f1f0b95cf9485185ab6b9a79b625
atef1232 <atefzahran642@gmail.com>
2025-03-21 17:16:06 +0200

    add two models users and product review

---
+++ Shopegy/Models/productreivew.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace Shopegy.Models
+{
+    public class productreivew
+    {
+        public int ReviewID { get; set; }
+        public int UserID { get; set; }
+        public string ProductID { get; set; }
+        public decimal Rating { get; set; }
+        public DateTime ReviewDate { get; set; } = DateTime.Now;
+    }
+}
---
+++ Shopegy/Models/users.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Shopegy.Models
+{
+    public class users
+    {
+       
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public bool IsAdmin { get; set; } = false;
+        public string? Preferences { get; set; }
+        public string? RememberToken { get; set; }
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+        public DateTime UpdatedAt { get; set; } = DateTime.Now;
+        public int? CartId { get; set; }
+    }
+}
