54b8c3e7b011274284a455abb176977c50c543ab
atef1232 <atefzahran642@gmail.com>
2025-05-06 21:15:51 +0300

    Refactor ShopController and add dynamic product views
    Refactored `ShopController` to use dependency injection and updated
    the `Index` action to fetch and display products from the database.
    Added a new `GetProducts` action to return a partial view for
    product data. Updated `Index.cshtml` to dynamically display
    products in a grid layout. Created a new `_ProductList.cshtml`
    partial view for rendering product lists. Added a comment block
    to `View.cshtml` with a link to MVC documentation.

--- Shopegy/Controllers/ShopController.cs
+++ Shopegy/Controllers/ShopController.cs
@@ -1,13 +1,48 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿//using Microsoft.AspNetCore.Mvc;
+
+//namespace Shopegy.Controllers
+//{
+//	public class ShopController : Controller
+//	{
+//		public IActionResult Index()
+//		{
+
+//			return View();
+//		}
+//	}
+//}
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Shopegy.Models;
+using Data;
 
 namespace Shopegy.Controllers
 {
-	public class ShopController : Controller
-	{
-		public IActionResult Index()
-		{
+    public class ShopController : Controller
+    {
+        private readonly ShopegyAppContext _context;
 
-			return View();
-		}
-	}
+        public ShopController(ShopegyAppContext context)
+        {
+            _context = context;
+        }
+
+        // Action لعرض كل المنتجات في الصفحة الرئيسية للمحل
+        public IActionResult Index()
+        {
+            var products = _context.Products
+                .Include(p => p.ProductCategorie) // لو حابب تجيب التصنيف مع المنتج
+                .ToList();
+
+            return View(products);
+        }
+        public IActionResult GetProducts()
+        {
+            var products = _context.Products
+                                   .Include(p => p.ProductCategorie)
+                                   .ToList();
+            return PartialView("_ProductList", products);
+        }
+
+    }
 }
